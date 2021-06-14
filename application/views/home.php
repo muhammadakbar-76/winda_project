694b1be2a3bf09ccc9f872161dc48a27656cadb4
Muhammad Akbar <muhammadakbar509@gmail.com>
2021-06-14 15:36:58 +0800

    - membuat section lokasi - menghapus bagian style.css yang tidak perlu

--- application/views/home.php
+++ application/views/home.php
@@ -9,3 +9,27 @@
 </div>
 <figure><h3>"Memberikan yang Terbaik"</h3></figure>
 </section>
+
+<section id="lokasi">
+<h2 class="text-center">Lokasi Kami</h2><hr>
+<div class="row">
+<div class="col-sm-8">
+<p style="font-family: monoscape; font-size: 20px;padding: 0 0 0 80px;">Jalan Ir. H. Pangeran Muhammad Noor No.99, Kuin Cerucuk, Kec. Banjarmasin Bar., Kota Banjarmasin, Kalimantan Selatan 70128</p>
+<div class="text-center">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3983.1786141466364!2d114.56789391428642!3d-3.3059459420947976!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2de4224d1fb938a5%3A0x77113382d09f2a5a!2sPT.%20Surya%20Satrya%20Timur%20Corp!5e0!3m2!1sid!2sid!4v1623653132320!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+</div>
+</div>
+<div class="col-sm-4" style="border-left: 2px solid grey;">
+    <h4 class="text-center">Kontak</h4><hr style="width: 25%;">
+    <div class="row">
+        <div class="col-12">
+            <p>&#128231; marketing2@sstc.co.id ; marketing@sstc.co.id</p>
+            <p>&#9742; (0511) 3354708, 4366275, 3352372</p>
+            <p>&#128439; (0511) 4368086</p>
+        </div>
+    </div>
+</div>
+</div>
+
+
+</section>
