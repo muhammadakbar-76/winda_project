ecef70d92e8ff67cfddc4577fa2a115c003a2f68
Muhammad Akbar <muhammadakbar509@gmail.com>
2021-06-14 12:06:01 +0800

    - Menghapus section kontak - Menghapus event script.js bagian section kontak - Mengganti info selengkapnya perusahaan menjadi 1 column saja

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ $('button.about').on('click', function(){
         $('.my-auto').remove();
         $('.aside').after(`<div class="col-sm-10 text-center"><h2 class="judul">Tentang Kami</h2><hr>
         <div class="row justify-content-center">
-        <div class="col-sm-10 pAtas"><p>Perusahaan PT. Surya Satrya Timur merupakan pabrik pengolahan kayu yang didirikan pada tahun 1976. Pabrik pengolahan ini terletak di Jln. Ir. PHM. Noor No. 99 (tepi Sungai Barito), Desa Kuin Cerucuk, Kecamatan Banjarmasin Barat, Kota Banjarmasin, Kalimantan Selatan, dan memiliki cabang kantor di jakarta yang berlokasi di Jl. Teluk Betung No. 45 E, Jakarta 10230, Indonesia.<br>
+        <div class="col-sm-10 pAtas"><p>Perusahaan PT. Surya Satrya Timur merupakan pabrik pengolahan kayu yang didirikan pada tahun 1976. Pabrik pengolahan ini terletak di Jln. Ir. PHM. Noor No. 99 (tepi Sungai Barito), Desa Kuin Cerucuk, Kecamatan Banjarmasin Barat, Kota Banjarmasin, Kalimantan Selatan, dan memiliki cabang kantor di jakarta yang berlokasi di Jl. Teluk Betung No. 45 E, Jakarta 10230, Indonesia.<br><br>
         PT. Surya Satrya Timur merupakan salah satu dari perusahaan di Indonesia yang memproduksi dan memasarkan berbagai jenis produk olahan kayu berkualitas tinggi. Beberapa jenis produk kami adalah General Plywood, Concrete Panel, Floorebase, Veneer, Blockboard, Barecore, Sawntimber, Polyester Plywood dan Polyester Blockboard. Saat ini kami merupakan salah satu pemain global di dunia dan telah mengekspor produk kami ke Jepang, Korea, Taiwan, India, Eropa dan USA. Seiring dengan perkembangan jaman saat ini, perusahaan pembuat kayu lapis dituntut untuk menghasilkan produk yang berstandar tinggi untuk dapat bersaing di pasar internasional. Tuntutan pembuatan produk yang bermutu ini dapat menimbulkan permasalahan baru yaitu masalah mengenai limbah.
         </p></div>
         </div></div>`);
@@ -17,28 +17,3 @@ $('button.about').on('click', function(){
     })
    
 });
-
-$('.close').on('click', function(){
-    $('.card').remove();
-})
-
-$("input[type='radio']").on('click',function(){
-   if ($("input[name='pilihan']:checked").val() == 'perusahaan') {
-       $("button[type='submit']").before(`<div class="form-perusahaan"><div class="form-group">
-       <label for="perusahaan">Nama Perusahaan</label>
-       <input type="text" name="perusahaan" class="form-control" placeholder="Nama Perusahaan Anda">
-     </div>
-     <div class="form-group">
-          <label for="alamat">Alamat</label>
-          <input type="text" name="alamat" class="form-control" placeholder="Alamat Perusahaan">
-        </div>
-        <div class="form-group">
-          <label for="izin">SIUP / NIB</label>
-          <input type="text" name="izin" class="form-control" placeholder="Nomor SIUP/NIB">
-        </div></div>`);
-   }
-
-   else {
-       $("div.form-perusahaan").remove();
-   }
-})
