4bd920f58c6001219250d7cd28a95cbbea2abba3
Muhammad Akbar <muhammadakbar509@gmail.com>
2021-12-25 15:07:18 +0800

    Something went missing, i forget to commit in last section

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -9,8 +9,8 @@
 </div>
 </footer>
 <script src="<?= base_url(); ?>assets/js/jquery-ui-1.7.2/jquery-3.5.0.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-<script src="<?= base_url(); ?>assets/js/bootstrap.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
 <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 <script>let base ='<?= base_url() ?>';
 </script>
